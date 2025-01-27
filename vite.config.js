2f97a0068a9a575d8ff45e725dff525b90c8f334
surya1604 <VASHISTH.SURYA33@GMAIL.COM>
2025-01-27 14:28:43 +0530

    final commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Ensure base is '/' unless hosting under a subpath
   build: {
     outDir: 'dist',
   },
